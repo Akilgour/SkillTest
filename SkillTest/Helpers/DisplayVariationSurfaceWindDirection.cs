79eb43b807d7312bb7d798b0ef894965602295d5
Ashley Kilgour <5243874+Akilgour@users.noreply.github.com>
2023-09-13 21:23:22 +0100

    Added DisplayVariationSurfaceWindDirection and its tests

---
+++ SkillTest/Helpers/DisplayVariationSurfaceWindDirection.cs
@@ -0,0 +1,25 @@
+﻿namespace Mma.Common.Helpers
+{
+    using Mma.Common.models;
+
+    public static class DisplayVariationSurfaceWindDirection
+    {
+        public static string Resolve(WindData windData)
+        {
+            var minimumWindDirection = "///";
+            var maximumWindDirection = "///";
+
+            if (windData.MinimumWindDirection != null)
+            {
+                minimumWindDirection = $"{windData.MinimumWindDirection:000}";
+            }
+
+            if (windData.MaximumWindDirection != null)
+            {
+                maximumWindDirection = $"{windData.MaximumWindDirection:000}";
+            }
+
+            return $"{minimumWindDirection}V{maximumWindDirection}";
+        }
+    }
+}
---
+++ Test.Mma.Common/Helpers/DisplayVariationSurfaceWindDirection_Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace Test.Mma.Common.Helpers
+{
+    using global::Mma.Common.Helpers;
+    using global::Mma.Common.models;
+    using NUnit.Framework;
+
+    internal class Display_variation_surface_wind_direction_tests
+    {
+        [TestCase(null, null, "///V///")]
+        [TestCase(200, null, "200V///")]
+        [TestCase(null, 300, "///V300")]
+        [TestCase(10, 20, "010V020")]
+        [TestCase(15, 1, "015V001")]
+        [TestCase(2, 25, "002V025")]
+        [TestCase(100, 350, "100V350")]
+        public void Wind_variation_is_correct(double? minimumWindDirection, double? maximumWindDirection, string expected)
+        {
+            //Arrange
+            var data = new WindData
+            {
+                MinimumWindDirection = minimumWindDirection,
+                MaximumWindDirection = maximumWindDirection,
+            };
+            //Act
+            var result = DisplayVariationSurfaceWindDirection.Resolve(data);
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
