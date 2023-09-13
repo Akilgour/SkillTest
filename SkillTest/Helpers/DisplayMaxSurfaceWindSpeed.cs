da3802c53a8e069fce104275c11e315f219afc23
Ashley Kilgour <5243874+Akilgour@users.noreply.github.com>
2023-09-13 20:51:07 +0100

    Added DisplayMaxSurfaceWindSpeed and its tests

---
+++ SkillTest/Helpers/DisplayMaxSurfaceWindSpeed.cs
@@ -0,0 +1,16 @@
+﻿namespace Mma.Common.Helpers
+{
+    using Mma.Common.models;
+
+    public static class DisplayMaxSurfaceWindSpeed
+    {
+        public static string Resolve(WindData windData)
+        {
+            if (windData.MaximumWindSpeed == null)
+            {
+                return "//";
+            }
+            return $"{windData.MaximumWindSpeed:00}";
+        }
+    }
+}
---
+++ Test.Mma.Common/Helpers/DisplayMaxSurfaceWindSpeed_Tests.cs
@@ -0,0 +1,26 @@
+﻿namespace Test.Mma.Common.Helpers
+{
+    using global::Mma.Common.Helpers;
+    using global::Mma.Common.models;
+    using NUnit.Framework;
+
+    public class Display_max_surface_wind_speed_Tests
+    {
+        [TestCase(null, "//")]
+        [TestCase(10, "10")]
+        [TestCase(15, "15")]
+        [TestCase(3, "03")]
+        public void Max_wind_speed_is_correct(double? speed, string expected)
+        {
+            //Arrange
+            var data = new WindData
+            {
+                MaximumWindSpeed = speed,
+            };
+            //Act
+            var result = DisplayMaxSurfaceWindSpeed.Resolve(data);
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
