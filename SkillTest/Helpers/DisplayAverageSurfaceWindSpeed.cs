be6231f5d4def77c18fcbc8cf1ea028167cb0324
Ashley Kilgour <5243874+Akilgour@users.noreply.github.com>
2023-09-13 20:34:13 +0100

    Added DisplayAverageSurfaceWindSpeed helper and its tests

---
+++ SkillTest/Helpers/DisplayAverageSurfaceWindSpeed.cs
@@ -0,0 +1,16 @@
+﻿namespace Mma.Common.Helpers
+{
+    using Mma.Common.models;
+
+    public static class DisplayAverageSurfaceWindSpeed
+    {
+        public static string Resolve(WindData windData)
+        {
+            if (windData.AverageWindSpeed == null)
+            {
+                return "//";
+            }
+            return $"{windData.AverageWindSpeed:00}";
+        }
+    }
+}
---
+++ Test.Mma.Common/Helpers/DisplayAverageSurfaceWindSpeed_Test.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Test.Mma.Common.Helpers
+{
+    using global::Mma.Common.Helpers;
+    using global::Mma.Common.models;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class Display_Average_Surface_WindSpeed_Test
+    {
+        [TestCase(null, "//")]
+        [TestCase(10, "10")]
+        [TestCase(15, "15")]
+        [TestCase(3, "03")]
+        public void Average_wind_speed_is_correct(double? speed, string expected)
+        {
+            //Arrange
+            var data = new WindData
+            {
+                AverageWindSpeed = speed,
+               
+            };
+            //Act
+            var result = DisplayAverageSurfaceWindSpeed.Resolve(data);
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
