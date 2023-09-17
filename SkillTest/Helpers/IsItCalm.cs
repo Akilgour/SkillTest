6070b1491add765398983de7b82ddee27204917c
Ashley Kilgour <5243874+Akilgour@users.noreply.github.com>
2023-09-18 00:50:58 +0100

    Added a IsCalm

---
+++ SkillTest/Helpers/IsItCalm.cs
@@ -0,0 +1,12 @@
+﻿namespace Mma.Common.Helpers
+{
+    using Mma.Common.models;
+
+    public static class IsItCalm
+    {
+        //  When the wind speed is less than 1 knot, this should be reported as calm
+
+        public static bool Resolve(WindData windData) =>
+            windData.AverageWindSpeed < 1;
+    }
+}
---
+++ Test.Mma.Common/Helpers/IsItCalmTest.cs
@@ -0,0 +1,46 @@
+﻿using Mma.Common.Helpers;
+using Mma.Common.models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Mma.Common.Helpers
+{
+    internal class IsItCalmTest
+    {
+        [Theory]
+        [TestCase(0 )]
+        [TestCase(0.1 )]
+        [TestCase(0.9 )]
+        public void It_is_calm_wind_speed_less_than_one_knot(double? averageWindSpeed)
+        {
+            //Arrange
+            var data = new WindData
+            {
+               AverageWindSpeed = averageWindSpeed,
+            };
+            //Act
+            var result = IsItCalm.Resolve(data);
+            //Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void It_is_calm_wind_speed_one_knot_or_more(double? averageWindSpeed)
+        {
+            //Arrange
+            var data = new WindData
+            {
+                AverageWindSpeed = averageWindSpeed,
+            };
+            //Act
+            var result = IsItCalm.Resolve(data);
+            //Assert
+            Assert.False(result);
+        }
+    }
+}
