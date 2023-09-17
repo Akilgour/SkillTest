1673a6971f819fef15943f3f16b8f94b1c54300e
Ashley Kilgour <5243874+Akilgour@users.noreply.github.com>
2023-09-18 00:05:44 +0100

    Added loggice for  DisplayAverageSurfaceWindDirection so it follows The average wind direction shall NOT be included for variable winds when the total variation in direction over the previous ten - minute period   is 60 degrees or more or     but less than 180 degrees and the wind speed is 3 knots or less; the wind in this case shall be reported as variable.

--- SkillTest/Helpers/DisplayAverageSurfaceWindDirection.cs
+++ SkillTest/Helpers/DisplayAverageSurfaceWindDirection.cs
@@ -6,11 +6,27 @@
     {
         public static string Resolve(WindData windData)
         {
-            if (windData.AverageWindDirection == null)
+            // AK I am not 100% sure on where there -or- and -and- are in this sentance.
+            // So I would go find a product owner, and just have a chat
+
+            //The average wind direction shall NOT be included
+            //for variable winds when the total variation in direction over the previous ten - minute period
+            //  is 60 degrees or more
+            //or
+            //    but less than 180 degrees
+            //and the wind speed is 3 knots or less; the wind in this case shall be reported as variable.
+
+            var variationInDirection = windData.MaximumWindDirection - windData.MinimumWindDirection;
+
+            if (variationInDirection <= 60 || variationInDirection > 180)
             {
-                return "///";
+                if (windData.AverageWindSpeed <= 3)
+                {
+                    return $"{windData.AverageWindDirection:000}"; ;
+                }
             }
-            return $"{windData.AverageWindDirection:000}";
+
+            return "VRB";
         }
     }
 }
--- Test.Mma.Common/Helpers/DisplayAverageSurfaceWindDirection_Tests.cs
+++ Test.Mma.Common/Helpers/DisplayAverageSurfaceWindDirection_Tests.cs
@@ -7,22 +7,178 @@
     [TestFixture]
     public class Display_average_surface_wind_direction_tests
     {
-
-        [TestCase(null, "///")]
-        [TestCase(10, "010")]
-        [TestCase(15, "015")]
-        [TestCase(350, "350")]
-        public void Average_wind_direction_is_correct(double? direction, string expected)
+        [Theory]
+        [TestCase(10, 20, 15, 0, "015")]
+        [TestCase(10, 20, 15, 1, "015")]
+        [TestCase(10, 20, 15, 2, "015")]
+        [TestCase(10, 20, 15, 3, "015")]
+        [TestCase(10, 30, 20, 0, "020")]
+        [TestCase(10, 30, 20, 1, "020")]
+        [TestCase(10, 30, 20, 2, "020")]
+        [TestCase(10, 30, 20, 3, "020")]
+        [TestCase(10, 40, 25, 0, "025")]
+        [TestCase(10, 40, 25, 1, "025")]
+        [TestCase(10, 40, 25, 2, "025")]
+        [TestCase(10, 40, 25, 3, "025")]
+        [TestCase(10, 50, 30, 0, "030")]
+        [TestCase(10, 50, 30, 1, "030")]
+        [TestCase(10, 50, 30, 2, "030")]
+        [TestCase(10, 50, 30, 3, "030")]
+        [TestCase(10, 60, 35, 0, "035")]
+        [TestCase(10, 60, 35, 1, "035")]
+        [TestCase(10, 60, 35, 2, "035")]
+        [TestCase(10, 60, 35, 3, "035")]
+        [TestCase(10, 70, 40, 0, "040")]
+        [TestCase(10, 70, 40, 1, "040")]
+        [TestCase(10, 70, 40, 2, "040")]
+        [TestCase(10, 70, 40, 3, "040")]
+        [TestCase(20, 80, 50, 0, "050")]
+        [TestCase(20, 80, 50, 1, "050")]
+        [TestCase(20, 80, 50, 2, "050")]
+        [TestCase(20, 80, 50, 3, "050")]
+        public void Average_wind_direction_is_shown_as_varation_is_60_degrees_or_less_and_wind_speed_3_knots_or_less(double? minimumWindDirection, double? maximumWindDirection, double? averageWindDirection, double? averageWindSpeed, string expected)
         {
             //Arrange
             var data = new WindData
             {
-                AverageWindDirection = direction,
+                MinimumWindDirection = minimumWindDirection,
+                MaximumWindDirection = maximumWindDirection,
+                AverageWindDirection = averageWindDirection,
+                AverageWindSpeed = averageWindSpeed,
             };
             //Act
             var result = DisplayAverageSurfaceWindDirection.Resolve(data);
             //Assert
             Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Theory]
+        [TestCase(10, 20, 15, 3.1)]
+        [TestCase(10, 20, 15, 4)]
+        [TestCase(10, 30, 20, 3.1)]
+        [TestCase(10, 30, 20, 4)]
+        [TestCase(10, 40, 25, 3.1)]
+        [TestCase(10, 40, 25, 4)]
+        [TestCase(10, 50, 30, 3.1)]
+        [TestCase(10, 50, 30, 4)]
+        [TestCase(10, 60, 35, 3.1)]
+        [TestCase(10, 60, 35, 4)]
+        [TestCase(10, 70, 40, 3.1)]
+        [TestCase(10, 70, 40, 4)]
+        [TestCase(20, 80, 50, 3.1)]
+        [TestCase(20, 80, 50, 4)]
+        public void Average_wind_direction_is_VRB_as_varation_is_60_degrees_or_less_and_wind_speed_more_than_3_knots(double? minimumWindDirection, double? maximumWindDirection, double? averageWindDirection, double? averageWindSpeed)
+        {
+            //Arrange
+            var data = new WindData
+            {
+                MinimumWindDirection = minimumWindDirection,
+                MaximumWindDirection = maximumWindDirection,
+                AverageWindDirection = averageWindDirection,
+                AverageWindSpeed = averageWindSpeed,
+            };
+            //Act
+            var result = DisplayAverageSurfaceWindDirection.Resolve(data);
+            //Assert
+            Assert.That(result, Is.EqualTo("VRB"));
+        }
+
+        [Theory]
+        [TestCase(10, 191, 100, 1, "100")]
+        [TestCase(10, 191, 100, 2, "100")]
+        [TestCase(10, 191, 100, 3, "100")]
+        [TestCase(10, 200, 105, 1, "105")]
+        [TestCase(10, 200, 105, 2, "105")]
+        [TestCase(10, 200, 105, 3, "105")]
+        [TestCase(20, 220, 120, 1, "120")]
+        [TestCase(20, 220, 120, 2, "120")]
+        [TestCase(20, 220, 120, 3, "120")]
+        [TestCase(20, 220, 120, 1, "120")]
+        [TestCase(20, 220, 120, 2, "120")]
+        [TestCase(20, 220, 120, 3, "120")]
+        public void Average_wind_direction_is_shown_as_varation_is_180_degrees_or_more_and_wind_speed_3_knots_or_less(double? minimumWindDirection, double? maximumWindDirection, double? averageWindDirection, double? averageWindSpeed, string expected)
+        {
+            //Arrange
+            var data = new WindData
+            {
+                MinimumWindDirection = minimumWindDirection,
+                MaximumWindDirection = maximumWindDirection,
+                AverageWindDirection = averageWindDirection,
+                AverageWindSpeed = averageWindSpeed,
+            };
+            //Act
+            var result = DisplayAverageSurfaceWindDirection.Resolve(data);
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Theory]
+        [TestCase(10, 190, 100, 3.1)]
+        [TestCase(10, 190, 100, 4)]
+        [TestCase(10, 200, 105, 3.1)]
+        [TestCase(10, 200, 105, 4)]
+        [TestCase(20, 220, 120, 3.1)]
+        [TestCase(20, 220, 120, 4)]
+        [TestCase(20, 220, 120, 3.1)]
+        [TestCase(20, 220, 120, 4)]
+        public void Average_wind_direction_is_VRB_as_varation_is_180_degrees_or_more_and_wind_speed_more_than_3_knots(double? minimumWindDirection, double? maximumWindDirection, double? averageWindDirection, double? averageWindSpeed)
+        {
+            //Arrange
+            var data = new WindData
+            {
+                MinimumWindDirection = minimumWindDirection,
+                MaximumWindDirection = maximumWindDirection,
+                AverageWindDirection = averageWindDirection,
+                AverageWindSpeed = averageWindSpeed
+            };
+            //Act
+            var result = DisplayAverageSurfaceWindDirection.Resolve(data);
+            //Assert
+            Assert.That(result, Is.EqualTo("VRB"));
+        }
+
+        [Theory]
+        [TestCase(10, 71, 3.1)]
+        [TestCase(10, 71, 4)]
+        [TestCase(10, 80, 3.1)]
+        [TestCase(10, 80, 4)]
+        [TestCase(10, 189, 3.1)]
+        [TestCase(10, 189, 4)]
+        public void Average_wind_direction_is_VRB_as_varation_is_between_60_180_degrees_and_wind_speed_more_than_3_knots(double? minimumWindDirection, double? maximumWindDirection, double? averageWindSpeed)
+        {
+            //Arrange
+            var data = new WindData
+            {
+                MinimumWindDirection = minimumWindDirection,
+                MaximumWindDirection = maximumWindDirection,
+                AverageWindSpeed = averageWindSpeed
+            };
+            //Act
+            var result = DisplayAverageSurfaceWindDirection.Resolve(data);
+            //Assert
+            Assert.That(result, Is.EqualTo("VRB"));
+        }
+
+        [Theory]
+        [TestCase(10, 71, 2)]
+        [TestCase(10, 71, 3)]
+        [TestCase(10, 80, 2)]
+        [TestCase(10, 80, 3)]
+        [TestCase(10, 189, 2)]
+        [TestCase(10, 189, 3)]
+        public void Average_wind_direction_is_VRB_as_varation_is_between_60_180_degrees_and_wind_speed_is_3_knots_or_less(double? minimumWindDirection, double? maximumWindDirection, double? averageWindSpeed)
+        {
+            //Arrange
+            var data = new WindData
+            {
+                MinimumWindDirection = minimumWindDirection,
+                MaximumWindDirection = maximumWindDirection,
+                AverageWindSpeed = averageWindSpeed
+            };
+            //Act
+            var result = DisplayAverageSurfaceWindDirection.Resolve(data);
+            //Assert
+            Assert.That(result, Is.EqualTo("VRB"));
         }
     }
 }
